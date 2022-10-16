75209de9bd4bdd27943c70ad6c8765db6edbb890
= <=>
2022-10-17 02:10:57 +0530

    remote db added

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 // init database
-
+//t8vb1Ogd6Oa0WQTr
+//sachin
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-const url = 'mongodb://localhost:27017';
+const url = `mongodb+srv://sachin:t8vb1Ogd6Oa0WQTr@cluster0.9tvqwmp.mongodb.net/?retryWrites=true&w=majority`;
 const databaseName = 'mixingApp';
 const client = new MongoClient(url, { useNewUrlParser: true });
 const db = client.db(databaseName);
