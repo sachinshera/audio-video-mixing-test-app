ec9ec0821b2ec2e76ef67c6c696403aef023cb26
= <=>
2022-10-17 02:20:12 +0530

    port changes for heroku

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
     });
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
